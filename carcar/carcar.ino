d26f38c18523f921c0e564766b9e5d6ac552334c
zououming <zououming@gmail.com>
2020-07-12 22:33:16 +0800

    7.12

--- carcar/carcar.ino
+++ carcar/carcar.ino
@@ -1,23 +1,28 @@
-#define input1 3 // 定义uno的pin 5 向 input1 输出 1 2 控制左轮
-#define input2 4 // 定义uno的pin 6 向 input2 输出
-#define input3 5 // 定义uno的pin 9 向 input3 输出 3 4 控制右轮
-#define input4 6 // 定义uno的pin 10 向 input4 输出
+#define input1 3 // pin3 4 输出 input1 2 控制左轮
+#define input2 4
 
-#define leftpow 2 // 左轮使能
-#define rightpow 7
+#define input3 5 // pin5 6 输出 input3 4 控制右轮
+#define input4 6
+
+#define leftpow 2   // 左轮使能
+#define rightpow 7  // 右轮使能
 
 #define right_trig 10 // 右侧超声波
 #define right_echo 11
+
 #define left_trig 13  // 左侧超声波
 #define left_echo 12
 
-#define ultrasound_lenth 1
-#define turn_delay 3000           // 转向延迟
-#define adj_dis 5                 // 调整方向距离阈值
-#define adj_num 3
-#define turn_dis 100              // 判断拐角距离阈值
+#define turn_left_delay 750            // 转向延迟
+#define turn_right_delay 800
+#define adj_dis 2                 // 调整方向距离阈值
+#define adj_num 1                 // 调整方向条件
 #define adj_delay 20              // 调整方向延迟
-#define send_delay 30            // 接受数据等待时间
+#define turn_dis 30               // 判断拐角距离阈值
+#define send_delay 30             // 接受数据等待时间
+#define corner_delay 250          // 进入拐角的延迟 
+#define after_turn_stop_delay 100 
+#define after_turn_delay 1000      // 转向后延迟
 
 #define left 0
 #define right 1
@@ -26,24 +31,12 @@ float now_distance[2] = {0};  // 左和右
 float last_distance[2] = {0};
 bool init_flag = true;
 
-float turn_distance;//turn_distance's problem
-int default_direction = right;
+float turn_distance;
+int default_direction = left;
 int default_trig;
 int default_echo;
 
-float dis_left[ultrasound_lenth];
-float dis_right[ultrasound_lenth];
-
-float avg(float a[ultrasound_lenth])
-{
-  float b = 0;
-  for (int i = 0; i < ultrasound_lenth; i++)
-  {
-    b += a[i];
-  }
-  b = b / 10;
-  return b;
-}
+String receive_frame;
 
 void go_straight()
 {
@@ -51,8 +44,8 @@ void go_straight()
   digitalWrite(input2, LOW); //给低电平
   digitalWrite(input3, LOW); //给高电平
   digitalWrite(input4, HIGH); //给低电平
-  analogWrite(leftpow, 100);//左 100 6V
-  analogWrite(rightpow, 118);//右 116
+  analogWrite(leftpow, 80);//左 100 6V
+  analogWrite(rightpow, 90);//右 116
 }
 
 void stop()
@@ -63,24 +56,20 @@ void stop()
   digitalWrite(input4, LOW);
 }
 
-void Tright()//右转
+void Tleft()
 {
-  digitalWrite(input1, HIGH);
-  digitalWrite(input2, LOW); 
-  digitalWrite(input3, HIGH); 
-  digitalWrite(input4, LOW); 
-  analogWrite(leftpow, 160);//左 160 6V
-  analogWrite(rightpow, 170);//右 170
-}
-
-void Tleft()//左转
-{
-  digitalWrite(input1, LOW); 
+  digitalWrite(input1, LOW);
   digitalWrite(input2, HIGH); 
   digitalWrite(input3, LOW); 
   digitalWrite(input4, HIGH); 
-  analogWrite(leftpow, 160);//左 100 6V
-  analogWrite(rightpow, 170);//右 116
+}
+
+void Tright()
+{
+  digitalWrite(input1, HIGH); 
+  digitalWrite(input2, LOW); 
+  digitalWrite(input3, HIGH); 
+  digitalWrite(input4, LOW); 
 }
 
 bool adjust_direction()
@@ -91,7 +80,7 @@ bool adjust_direction()
 //  Serial.print("    dis: ");
 //  Serial.println(turn_distance);
 
-  if ( (error < -adj_dis && error > -turn_dis) || (now_distance[default_direction] < 5 && now_distance[default_direction] > 0) ){
+  if ( (error < -adj_dis && error > -turn_dis) || (now_distance[default_direction] < 3 && now_distance[default_direction] > 0) ){
     if (default_direction == right)
       Adj_left();
     else if (default_direction == left)
@@ -109,11 +98,15 @@ void Adj_left()
 {
   int count = 0;
   Tleft();
+  analogWrite(leftpow, 100);//左 160 6V
+  analogWrite(rightpow, 110);//右 170
   while (count < adj_num){
     delay(adj_delay);
     update_distance();
     if (last_distance[default_direction] - now_distance[default_direction] < 0)
       count++;
+    else if (last_distance[default_direction] - now_distance[default_direction] > turn_dis)
+      break;
   }
   turn_distance = now_distance[default_direction];
   go_straight();
@@ -123,11 +116,15 @@ void Adj_right()
 {
   int count = 0;
   Tright();
+  analogWrite(leftpow, 100);//左 160 6V
+  analogWrite(rightpow, 110);//右 170
   while (count < adj_num){
     delay(adj_delay);
     update_distance();
     if (last_distance[default_direction] - now_distance[default_direction] < 0)
       count++;
+    else if (last_distance[default_direction] - now_distance[default_direction] > turn_dis)
+      break;      
   }
   turn_distance = now_distance[default_direction];
   go_straight();
@@ -176,6 +173,7 @@ float Ultrasonic(int Trig, int Echo)
   cm = (temp * 1 ) / 58;
   if (cm > 300)
     cm = -1;
+
   //  Serial.print(Echo);
   //  Serial.println();
   //  Serial.print(temp);
@@ -189,18 +187,12 @@ bool is_corner()
 {
 //  if (now_distance[left] == -1 || now_distance[right] == -1)
 //    return 1;
-  if (now_distance[left] - last_distance[left] > turn_dis)
+  if (now_distance[left] - last_distance[left] >= turn_dis)
     return 1;
-  if (now_distance[right] - last_distance[right] > turn_dis)
+  if (now_distance[right] - last_distance[right] >= turn_dis)
     return 1;
+    
   return 0;
-}
-
-void clear_buffer()
-{
-  char var;
-  while( Serial.available() )
-    var = Serial.read();
 }
 
 char Send()
@@ -209,30 +201,41 @@ char Send()
   delay(send_delay);
 }
 
+void clear_buffer()
+{
+  while (Serial.available())
+    Serial.read();
+}
+
 String Uart()
 {
   char a, b;
   String c;
+
   while (Serial.available())
   {
     a = Serial.read();
-    if (a == '&')
+    if (a == '&'){
+      receive_frame = "";
       while (Serial.available())
       {
         b = Serial.read();
         if (b == '#')
-          break;
+          break; 
         c += b;
       }
+      Serial.flush();
+//      clear_buffer();
+    }
   }
-  //    Serial.println("arduino: "+ c);
+   
   return c;
 }
 
 void turn()
 {
   String response;
-  
+  delay(corner_delay);
   stop();
   do {
     Send();
@@ -240,33 +243,38 @@ void turn()
   }
   while (response.length() == 0);
   
-
   if (response == "0") //左转
   {
     Tleft();
-    delay(turn_delay);
+    analogWrite(leftpow, 160);//左 160 6V
+    analogWrite(rightpow, 170);//右 170
+    delay(turn_left_delay);
     default_direction = right;
   }
   else if (response == "1") //右转
   {
     Tright();
-    delay(turn_delay);
+    analogWrite(leftpow, 160);//左 160 6V
+    analogWrite(rightpow, 170);//右 170
+    delay(turn_right_delay);
     default_direction = left;
   }
-  else
+  else// if (response == "2" || response == "3")
   {
-    stop();
-    while(1);
+    while(1)
+      stop();
   }
-  
-  clear_buffer();
+  stop();
+  delay(after_turn_stop_delay);
+  go_straight();
+  delay(after_turn_delay);
+
   update_distance();
   turn_distance = now_distance[default_direction];
 }
 
 void setup() {
   Serial.begin (9600);
-  //初始化各IO,模式为OUTPUT 输出模式
   pinMode(input1, OUTPUT);
   pinMode(input2, OUTPUT);
   pinMode(input3, OUTPUT);
@@ -276,23 +284,26 @@ void setup() {
   pinMode(left_trig, OUTPUT);
   pinMode(left_echo, INPUT);
   
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < 5; i++)
     update_distance();
     
   go_straight();
   update_distance();
-//  Serial.println(now_distance[0]);
   turn_distance = now_distance[default_direction];
 }
 
 void loop() {
   go_straight();
   update_distance();
+//  Uart();
+  if (is_corner())
+    turn();
   adjust_direction();
-//  if (is_corner())
-//    turn();
 
-  //   for(int i=0;i<10;i++)
-  
-  //    djud2[i]=Ultrasonic(left_trig,left_echo);
+//   Tright();
+//   delay(turn_delay);
+//   analogWrite(leftpow, 160);//左 160 6V
+//   analogWrite(rightpow, 170);//右 170
+//   stop();
+//   delay(after_turn_delay);
 }
